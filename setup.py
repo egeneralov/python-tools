9f0bb5bdc36030d6fa320b47a63fdd1b8c781d95
Eduard Generalov <eduard@generalov.net>
2018-05-11 00:43:37 +0300

    Fix dependencies

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
   name='python-tools',
-  version='0.0.5',
+  version='0.0.6',
   license='MIT',
   packages=find_packages(),
   platforms='any',
   zip_safe=False,
   include_package_data=True,
+
   author='Eduard Generalov',
   author_email='eduard@generalov.net',
   description='Some tools for python (sites parsing)',
+
+  install_requires=[
+    'pika',
+    'geopy',
+    'geocoder'
+  ],
+
   entry_points = {
     'console_scripts': [
       'tools = tools:push_id',
